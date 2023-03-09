23711df8c197ebf419775ddfdde5d3889f0661ff
AlexTolstov? <Tolstov777Alex@yandex.ru>
2023-03-09 20:52:56 +0700

    First Commit

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+//тест номер: я сбился со счёта(
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        
+    }
+}
